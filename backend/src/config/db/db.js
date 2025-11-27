975d4fc832f693ee056bdda178712ce50cdbe392
vks-g <gokulvks00@gmail.com>
2025-11-27 10:19:04 +0530

    updated prisma syntax acc to new update

--- backend/src/config/db/db.js
+++ backend/src/config/db/db.js
@@ -1,5 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
+const dotenv = require('dotenv');
+dotenv.config()
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
 
 module.exports = { prisma };
